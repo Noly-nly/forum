fc8583e913db5d1adf891b05f922b7b86f22e8f6
Noly <nuoleyi@outlook.com>
2023-03-29 20:10:25 +0800

    首次提交

--- src/main/java/com/noly/forum/config/MailConfiguration.java
+++ src/main/java/com/noly/forum/config/MailConfiguration.java
@@ -1,5 +1,6 @@
 package com.noly.forum.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,32 @@ import java.util.Properties;
 @Configuration
 public class MailConfiguration {
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.126.com");
-        mailSender.setPort(465);
+    @Value("${spring.mail.host}")
+    private String host;
 
-        mailSender.setUsername("nuoleyi@126.com");
-        mailSender.setPassword("Hu#0228126");
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtps");
+        props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
 
         return mailSender;
     }
--- src/test/java/com/noly/forum/MailTests.java
+++ src/test/java/com/noly/forum/MailTests.java
@@ -3,10 +3,13 @@ package com.noly.forum;
 import com.noly.forum.util.MailClient;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = ForumApplication.class)
 public class MailTests {
