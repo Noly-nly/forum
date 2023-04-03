2cfeeefe7de0513f9fec0197b236a115edc14151
Noly <nuoleyi@outlook.com>
2023-04-03 22:26:08 +0800

    开发社区核心功能：过滤敏感词

---
+++ src/test/java/com/noly/forum/SensitiveTests.java
@@ -0,0 +1,26 @@
+package com.noly.forum;
+
+import com.noly.forum.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = ForumApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter(){
+        String text = "哈哈哈哈哈·吸&毒·可以开&票，快来";
+        String filterText = sensitiveFilter.filter(text);
+        System.out.println(filterText);
+
+    }
+}
